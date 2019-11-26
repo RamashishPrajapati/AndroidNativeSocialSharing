880a9b3fdb0545b580fba5f1567ad50942c9818e
Ramashish Prajapati <ramashishprajapati22@gmail.com>
2019-11-26 13:52:21 +0530

    Changes the library name and file Updated the library file code

---
+++ projectlib/src/main/java/com/ram/projectlib/utils/CGlobal_Constant.java
@@ -0,0 +1,10 @@
+package com.ram.projectlib.utils;
+
+/**
+ * Created by Ramashish Prajapati on 27-02-2019
+ **/
+public class CGlobal_Constant {
+    public static final String SHARED_PREFERENCE_PERSISTENT = "SHARED_PREFERENCE_PERSISTENT";
+
+
+}
